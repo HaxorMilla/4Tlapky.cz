82a42c2329ed1e4436613c15226e4fb52674051d
Jan-Müller <haxormilla14@gmail.com>
2026-04-10 07:38:52 +0200

    Initial commit

---
+++ src/data/config.ts
@@ -0,0 +1,48 @@
+﻿// Global variables
+export const siteLang = "cs-CZ";
+export const siteCurrency = "CZK";
+export const siteDomain = "4tlapky.cz";
+export const siteName = "";
+export const siteSlogan = "To nejlepši pro Vaše mazlíčky!";
+export const themeColor = "#FF0000";
+
+export const phone = {
+	href: "tel:+420777775202",
+	label: "777 775 202",
+};
+
+export const email = {
+	href: "mailto:4tlapkysro@email.cz",
+	label: "4tlapkysro@email.cz",
+};
+
+export const address = {
+	city: "🗺75131 Lipník nad Bečvou",
+	street: "📍nám. T. G. Masaryka 92/5",
+	zip: "",
+	state: "Česká republika",
+	country: "🌎️CZ",
+};
+
+export const socialMedia = {
+	facebook: {
+		url: "https://www.facebook.com/profile.php?id=61556337299771&locale=cs_CZ",
+		label: "Facebook",
+		icon: "facebook",
+	},
+	instagram: {
+		url: "https://www.instagram.com/chovatelky4tlapky",
+		label: "Instagram",
+		icon: "instagram",
+	},
+};
+
+export const hours = {
+	"📅Pondělí": "9:00 – 12:00 | 13:00 – 16:30",
+	"📅Úterý": "8:30 – 12:00 | 13:00 – 16:30",
+	"📅Středa": "8:30 – 12:00 | 13:00 – 16:30",
+	"📅Čtvrtek": "8:30 – 12:00 | 13:00 – 16:30",
+	"📅Pátek": "8:30 – 12:00 | 13:00 – 16:30",
+	"📅Sobota": "8:30 – 11:00",
+	"❌Neděle":			 "Zavřeno",
+};
---
+++ src/data/menus.js
@@ -0,0 +1,14 @@
+﻿export const menuMain = [
+	{
+		name: "Úvodní stránka",
+		url: "/",
+	},
+	{
+		name: "Naše výrobky",
+		url: "/nase-vyrobky",
+	},
+	{
+		name: "Kontakujte nás",
+		url: "/contact",
+	},
+];
